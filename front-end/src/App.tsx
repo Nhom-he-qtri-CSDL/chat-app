92d9d5b7aea0de98a59935d48bf76c075d8c73f8
dak-1306 <thd13062005@gmail.com>
2026-04-01 16:02:20 +0700

    feat: add UI components and hooks

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,63 +1,64 @@
 import "./App.css";
-import { useCallback } from "react";
+// import { useCallback } from "react";
 
 function App() {
-  const login = useCallback(() => {
-    if (
-      !window.google ||
-      !window.google.accounts ||
-      !window.google.accounts.oauth2
-    ) {
-      console.error("Google API chưa sẵn sàng");
-      return;
-    }
+  // const login = useCallback(() => {
+  //   if (
+  //     !window.google ||
+  //     !window.google.accounts ||
+  //     !window.google.accounts.oauth2
+  //   ) {
+  //     console.error("Google API chưa sẵn sàng");
+  //     return;
+  //   }
 
-    const oauth2 = window.google.accounts.oauth2;
-    const init = oauth2.initCodeClient;
-    if (typeof init !== "function") {
-      console.error("initCodeClient không khả dụng");
-      return;
-    }
+  //   const oauth2 = window.google.accounts.oauth2;
+  //   const init = oauth2.initCodeClient;
+  //   if (typeof init !== "function") {
+  //     console.error("initCodeClient không khả dụng");
+  //     return;
+  //   }
 
-    const client = init({
-      client_id:
-        "12980680565-mvs1uv3vs8p01l3go3mkjii19juoahqc.apps.googleusercontent.com",
-      scope:
-        "openid email profile https://www.googleapis.com/auth/user.birthday.read",
-      ux_mode: "popup",
-      callback: (resp: { code?: string; error?: string }) => {
-        console.log("CODE:", resp.code);
+  //   const client = init({
+  //     client_id:
+  //       "12980680565-mvs1uv3vs8p01l3go3mkjii19juoahqc.apps.googleusercontent.com",
+  //     scope:
+  //       "openid email profile https://www.googleapis.com/auth/user.birthday.read",
+  //     ux_mode: "popup",
+  //     callback: (resp: { code?: string; error?: string }) => {
+  //       console.log("CODE:", resp.code);
 
-        fetch("https://localhost:443/api/v1/auth/google/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "x-api-key":
-              "7c771f67-f243-4166-a5f6-3b508799f70a_gG21JYbriiAcDbs4",
-          },
-          credentials: "include",
-          body: JSON.stringify({
-            auth_code: resp.code,
-          }),
-        })
-          .then((res) => res.json())
-          .then((data) => console.log(data))
-          .catch((err) => console.error("Fetch error:", err));
-      },
-    });
+  //       fetch("https://localhost:443/api/v1/auth/google/login", {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //           "x-api-key":
+  //             "7c771f67-f243-4166-a5f6-3b508799f70a_gG21JYbriiAcDbs4",
+  //         },
+  //         credentials: "include",
+  //         body: JSON.stringify({
+  //           auth_code: resp.code,
+  //         }),
+  //       })
+  //         .then((res) => res.json())
+  //         .then((data) => console.log(data))
+  //         .catch((err) => console.error("Fetch error:", err));
+  //     },
+  //   });
 
-    if (!client || typeof client.requestCode !== "function") {
-      console.error("client.requestCode không khả dụng");
-      return;
-    }
+  //   if (!client || typeof client.requestCode !== "function") {
+  //     console.error("client.requestCode không khả dụng");
+  //     return;
+  //   }
 
-    client.requestCode();
-  }, []);
+  //   client.requestCode();
+  // }, []);
 
   return (
-    <div style={{ padding: 16 }}>
-      <button onClick={login}>Login with Google</button>
-    </div>
+    // <div style={{ padding: 16 }}>
+    //   <button onClick={login}>Login with Google</button>
+    // </div>
+    <p className="text-lg font-bold">Hello, World!</p>
   );
 }
 export default App;
--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -8,24 +8,32 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
+// import fs from "fs";
+// import path from "path";
+
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(
-        path.resolve(
-          "../../end-to-end_encrypted_messaging_app/internal/certs/localhost+2-key.pem",
-        ), // đường dẫn key
-      ),
-      cert: fs.readFileSync(
-        path.resolve(
-          "../../end-to-end_encrypted_messaging_app/internal/certs/localhost+2.pem",
-        ), // đường dẫn cert
-      ),
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    port: 5173,
   },
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync(
+  //       path.resolve(
+  //         "../../end-to-end_encrypted_messaging_app/internal/certs/localhost+2-key.pem",
+  //       ), // đường dẫn key
+  //     ),
+  //     cert: fs.readFileSync(
+  //       path.resolve(
+  //         "../../end-to-end_encrypted_messaging_app/internal/certs/localhost+2.pem",
+  //       ), // đường dẫn cert
+  //     ),
+  //   },
+  //   port: 5173,
+  // },
 });
